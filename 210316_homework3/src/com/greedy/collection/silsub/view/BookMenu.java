1784bb01b713b7d59baeabbd8f2bd9d3a14d75f6
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-03-16 01:57:55 +0900

    jl

---
+++ 210316_homework3/src/com/greedy/collection/silsub/view/BookMenu.java
@@ -0,0 +1,18 @@
+package com.greedy.collection.silsub.view;
+
+import java.util.Scanner;
+
+public class BookMenu {
+
+//	public BookMenu() {};
+	
+	public void menu() {
+		System.out.println("*** 도서 관리 프로그램 **");
+		System.out.println("1. 새 도서 추가");
+		Scanner sc = new Scanner(System.in);
+		
+	}
+	
+	
+	
+}
